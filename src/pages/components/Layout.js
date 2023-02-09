a16ab930cfcd4067bf72407a8db251fb7e132154
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-02-09 20:36:32 -0300

    Head inf

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -1,10 +1,16 @@
+import Head from "next/head"
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+
 
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <link rel='shortcut icon' href='/images/favicon.ico'/>
+                <title>Pokedex</title>
+            </Head>
             <Navbar />
             <main>
                 {children}
