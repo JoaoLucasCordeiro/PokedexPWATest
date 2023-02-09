6a415008408c4bbbf4ca46f213622900e5c5b8b1
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-02-09 19:29:14 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
