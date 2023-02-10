e55b235e4f968a11813fcdcc9c9ba6a9c07c4949
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-02-09 21:52:05 -0300

    First call to API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,38 @@
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+// função de chamada para API
+export async function getStaticProps() {
+  // variável que define o número máximo de pokemons
+  const pokemonsLimit = 100
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+
+  // requisição feita com o fetch
+  const res = await fetch(`${api}/?limit=${pokemonsLimit}`)
+  const data = await res.json()
+
+  // adicionando o index do pokemon
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+
+}
+
+export default function Home({ pokemons }) {
   return (
-    <h1>Pokedex</h1>
+    <div>
+      <h1>Pokedex</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
