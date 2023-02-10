242ef40c7082d5a8d491644711d1b114232af7ba
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-02-10 00:51:49 -0300

    About Pokemon

--- src/pages/pokemon/[pokemonid].js
+++ src/pages/pokemon/[pokemonid].js
@@ -2,7 +2,7 @@ export default function Pokemon({ pokemon }) {
     return (
         <>
             <h1>Sobre o pokemon</h1>
-            <p>Em breve..</p>
+            <p>Em breve...</p>
         </>
 
     )
