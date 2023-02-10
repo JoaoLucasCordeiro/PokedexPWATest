85555fd676f3ed9e84d0a217ac2383a97d1add5e
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-02-10 00:06:55 -0300

    teste deploy

--- src/pages/components/Card.js
+++ src/pages/components/Card.js
@@ -5,17 +5,9 @@ import styles from '../../styles/Card.module.css'
 export default function Card({ pokemon }) {
     return (
         <div className={styles.card}>
-            <Image
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-                width='128'
-                height='128'
-                alt={pokemon.name} />
-
-        
+           
             <h3 className={styles.title}>{pokemon.name}</h3>
-            <Link href='/' className={styles.btn}>
-                Detalhes
-            </Link>
+           
         </div>
     )
 }
